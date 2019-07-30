a7b4f27158aac3800bdfbee0f6b9d1fd5eacd81d
Ali pourahrab <ali.pourahrab@gmail.com>
2019-07-30 02:41:51 -0700

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -4,7 +4,17 @@ from django.contrib.auth.models import User
 
 
 class Expense(models.Model):
-	text=models.CharField(max_length=255)
-	date=models.DateTimeField()
-	amount=models.BigIntegerField()
-	user=models.ForeignKey(User, 'on_delete')
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User, 'on_delete')
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount= models.BigIntegerField()
+    user = models.ForeignKey(User , 'on_delete')
+    def __unicode__(self):
+        return "{}-{}".format(self.date,self.amount)	
