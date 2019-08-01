520879149a45d8a609f720ab4676e297898d9022
Ali pourahrab <ali.pourahrab@gmail.com>
2019-08-01 01:42:15 -0700

    submit/expense/ and submit/income web services

--- web/models.py
+++ web/models.py
@@ -1,6 +1,15 @@
 from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
+
+#ASK;why def doesn't work ?!
+
+class Token(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    token=models.CharField(max_length=48)
+    def __unicode__(self):
+        return "{}_token".format(self.user)
 
 
 class Expense(models.Model):
@@ -9,7 +18,7 @@ class Expense(models.Model):
     amount=models.BigIntegerField()
     user=models.ForeignKey(User, 'on_delete')
     def __unicode__(self):
-        return "{}-{}".format(self.date,self.amount)
+        return "{}-{}" .format(self.date,self.amount)
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
